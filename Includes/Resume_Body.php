e9677207c34adad1b2c564324eab9217f031d046
SimonLowther <simon@jlobservatory.com>
2020-08-20 12:55:01 +1200

    Removal of personal data from body

--- Includes/Resume_Body.php
+++ Includes/Resume_Body.php
@@ -3,74 +3,20 @@
 
     <div class="resume-text">
         <div class="resume-left">
-            <h3>Personal Summary</h3>
-            <p>I have worked the last 20 years as an airline pilot, the last 15 for Air New Zealand currently as a Captain on the Airbus A321.
-                As a member of the Airbus Standards and Training team I bring to any role real world experiance in leading complex projects
-                with many differing facets through to completion.
-            </p>
-            <p>I know that my external experience combined with a good work ethic will bring valuable skills to your business.</p>
-            <hr>
-            <h3>Programming Skills & Education</h3>
-
-            <div><img class="badge" src="Resources/oracle-certified-associate-java-se-8-programmer.png" alt="Oracle certified OCA">
-                <h4> 2020: Oracle Certified Associate (Java Programmer)</h4>
-                Certifacation obtained through Oracle University (Online) <br>
-            </div>
-
-            <h4> 2018-Current: Flexihubs</h4>
-            <p>Private company (sole trader) building and maintaining websites for various companies and entities including eCommerce and
-                webtools such as booking sites, calendars, mail servers and more.
-                <br><br> <i>Noteworthy: This is my own company. I am very proud of its accomplishments, and the way it has grown and helped the local community.
-                </i><br>
-                <ul>
-                    <li>Build Websites using HTML/Php/css/JavaScript/CMS (Wordpress)</li>
-                    <li>Maintain Server (Linux)</li>
-                    <li>Communicate with customers</li>
-                    <li>Administration of company</li>
-                </ul>
-
-                <h5>Website Examples</h5>
-                <ul>
-                    <li>8count.co.nz</li>
-                    <li>bookingsheet.co.nz</li>
-                    <li>rasnz.org.nz</li>
-                    <li>censtar.party</li>
-                </ul>
-
-            </p>
-            <br>
-
-            <h4>2016 Private Contract: Royal Astronomical Society </h4> <br>
-            Build and maintain membership database frontend and backend. The database is used for tracking member details and
-            status, plus sending out newsletters, journals and yearly invoices. <br>
-            <ul>
-                <li>Written in Java and VB.net</li>
-                <li>Maria and XML Database</li>
-                <li>Java FX for GUI</li>
-            </ul><br>
-            <hr>
-            <h3>Work History</h3>
-
-            <h4>2005 - Present: Air New Zealand - Pilot (Captain A320)</h4>
-            <ul>
-                <li>Check and Training Captain responsible for maintaining flight crew standards</li>
-                <li>Operate Airbus A320 on International and Local routes</li>
-                <li>Design recurrent training for flight crew</li>
-            </ul>
-
-            <h4>2001 - 2005: Airwork NZ Ltd - Pilot</h4>
-            <ul>
-                <li>Metro 23 - Captain</li>
-                <li>Training Captain, responsible for training of new crew</li>
-                <li>Ground instructor, responsible development of courses for new crew and recurrent training</li>
-            </ul>
-
-            <h4>1998 - 2001: Ardmore Flying School - Flight Instructor</h4>
+            <h3>Left Side</h3>
+            
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam culpa sit expedita ut? 
+            Dignissimos, accusantium deleniti doloremque suscipit explicabo quidem quisquam perspiciatis rem ab esse culpa ex excepturi officia quae.</p>
+            <p>Quam iusto laborum voluptatem id quis distinctio ipsum. Quia vero enim sed sunt ipsam rem ab 
+            numquam molestias aspernatur molestiae laborum sit, facere cumque tempore dolores, quos, quae iure esse.</p>
+            <p>Iusto expedita culpa quibusdam sint nostrum nemo esse quas in obcaecati, quidem numquam architecto voluptates. Illum, minus tenetur quae repellat vero neque maxime accusantium tempore, ratione dolorum quia labore eius.</p>
+            <p>Incidunt corporis explicabo est molestias modi sit hic voluptatibus nihil, nisi beatae non, 
+            accusamus labore ullam, accusantium excepturi harum inventore ex at. Quia laborum quas fugit tempore? Nostrum, excepturi sint?</p>
 
         </div>
 
         <div class="resume-right">
-            <h3>Programming Languages/Tools</h3>
+            <h3>Right Side</h3>
             <table>
                 <tr>
                     <td>
@@ -100,30 +46,7 @@
                         </ul>
                     </td>
                 </tr>
-                <tr>
-                    <td>
-                        <h4>Other Qualifacations</h4>
-                        <ul>
-                            <li>Air Transport Pilot Licence</li>
-                            <li>Airline Flight Examiner Rating</li>
-                            <li>Diploma in Aviation Instruction</li>
-                            <li>Cat C & D flight instructor</li>
-
-                        </ul>
-                    </td>
-                </tr>
-            </table>
-            <h4>Noteworthy Interest and Achievments</h4>
-            I have had a keen interest in astronomy from a young age which continues today. I have created a research observatory in my backyard with which
-            I have contributed data to several significant projects and co-authored several papers published in noteworthy journals such as Astronomy and Astrophysics, and
-            Astrophysical Journal. Prints of these are available on request.
-            <h4>Items of Note</h4>
-            <ul>
-                <li>Live in Pukekohe</li>
-                <li>Married 18 years</li>
-                <li>Father of 2</li>
-                <li>Drivers Licence</li>
-            </ul>
+                
         </div>
     </div>
 </body>
